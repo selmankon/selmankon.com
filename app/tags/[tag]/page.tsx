17301654736e037e591bfa003605f57b52f63c75
selmankon <12685802+selmankon@users.noreply.github.com>
2025-01-15 01:29:12 +0300

    hoping last fix

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -29,9 +29,11 @@ export async function generateMetadata(props: {
 export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
-  return tagKeys.length > 0 ? tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
-  })) : [{ tag: 'default' }]
+  return tagKeys.length > 0
+    ? tagKeys.map((tag) => ({
+        tag: encodeURI(tag),
+      }))
+    : [{ tag: 'default' }]
 }
 
 export default async function TagPage(props: { params: Promise<{ tag: string }> }) {
