df2e8673cb246100bab026af45f29b159d409650
selmankon <12685802+selmankon@users.noreply.github.com>
2025-01-15 18:07:36 +0300

    fix: new about texts and tables

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -40,7 +40,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="bluesky" href={bluesky} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2 text-justify">
             {children}
           </div>
         </div>
