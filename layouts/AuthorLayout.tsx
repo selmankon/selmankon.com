66eb00eeeade685e3b79aa96e7dbe12b85f6c6e0
selmankon <12685802+selmankon@users.noreply.github.com>
2025-01-15 18:26:16 +0300

    fix: eslint rules

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -40,9 +40,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="bluesky" href={bluesky} />
             </div>
           </div>
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2 text-justify">
-            {children}
-          </div>
+          <div className="prose pt-6 text-justify dark:prose-invert xl:col-span-2">{children}</div>
         </div>
       </div>
     </>
